d1f3b2d0f2fbcee9e7a6848f831a030d2809bb15
Juan Montero <monteroespina@hotmail.com>
2024-01-03 18:00:52 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an API to assist you with all your pokemon needs.
--- routes/raids.js
+++ routes/raids.js
@@ -15,6 +15,7 @@ router.get('/:pokemonName', (req, res) => {
     else{
         res.status(404).send('pokemon not found '+pokemonName);
     }
+    
 
     /*
     Clefable
